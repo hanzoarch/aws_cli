fb7e985920e3996072e901bf5df61b20f1456839
kazuqueserasera <kazu.queserasera@gmail.com>
2018-08-20 15:57:16 +0900

    Update cloudtrail-enabled.sh

--- describe-config-rules/cloudtrail-enabled.sh
+++ describe-config-rules/cloudtrail-enabled.sh
@@ -1,3 +1,2 @@
-
-aws configservice describe-config-rules --config-rule-names $configrulenames --profile $username --output json
-
+#  cd /var/lib/jenkins/workspace/TrigerJob_Test/
+aws configservice describe-config-rules --config-rule-names $configrulenames --profile $username --output json --query "ConfigRules[].{ConfigRuleState:ConfigRuleState}" > result.txt
